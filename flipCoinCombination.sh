c3f1758bfcc02e349e0894d50b93c5374b4f0e2b
unknown <fddxzf@gmail.com>
2020-10-07 22:15:34 +0530

    store the singlet values and display percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -9,3 +9,22 @@
    else
      echo "its tails"
    fi
+   #stimulate the coin flip and store singlet combinations
+   declare -A coinflip
+   read -p "how many times do you want to flip coin :" number
+   counter=0
+   coinflip[H]=0
+   coinflip[T]=0
+   while (( $counter != $number ))
+   do
+   if [[ $((RANDOM%2)) -eq 1 ]]
+   then 
+        coinflip[H]=$((coinflip[H]+1))
+   else
+       coinflip[T]=$((coinflip[T]+1))
+   fi
+   counter=$(($counter+1))
+   done
+   echo "${coinflip[H]} $number"|awk '{print $1/$2}'
+   echo "${coinflip[T]} $number"|awk '{print $1/$2}'
+
