af28043e976ff83bb1f354923d2370084deb42f6
unknown <fddxzf@gmail.com>
2020-10-07 23:36:32 +0530

    compute doublets and store them

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -27,4 +27,30 @@
    done
    echo "${coinflip[H]} $number"|awk '{print $1/$2}'
    echo "${coinflip[T]} $number"|awk '{print $1/$2}'
-
+   #stimulate coin flip and stare double combinations
+   coinflip[HH]=0
+   coinflip[TT]=0
+   coinflip[TH]=0
+   coinflip[HT]=0
+   counter=0
+   while (( $counter != $number ))
+   do
+   if [[ $((RANDOM%2)) -eq 1 ]]
+   then
+       if [[ $((RANDOM%2)) -eq 1 ]]
+       then
+         coinflip[HH]=$((coinflip[HH]+1))
+       else
+         coinflip[HT]=$((coinflip[HT]+1))
+       fi
+   else
+      if [[ $((RANDOM%2)) -eq 1 ]]
+      then
+       coinflip[TH]=$((coinflip[TH]+1))
+      else
+       coinflip[TT]=$((coinflip[TT]+1))
+      fi
+   fi
+   counter=$(($counter+1))
+   done
+   
