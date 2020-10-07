2be823ae99cbbdb181ac882749e83a81668a8294
unknown <fddxzf@gmail.com>
2020-10-07 21:43:05 +0530

    stimulating the coin flip

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
    echo "welcome to flip coin combination code"
+   
+   #stimulate a coin flip
+   if [[ $((RANDOM%2)) -eq 1 ]]
+   then
+     echo "its head's"
+   else
+     echo "its tails"
+   fi
